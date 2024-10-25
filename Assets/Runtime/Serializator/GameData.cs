4a14b66284eac3b970f22d5d0ac81af5834e8817
Viktor Yurov <yurowm@gmail.com>
2024-10-25 14:26:50 +0700

    moved to Assets folder

--- Assets/Runtime/Serializator/GameData.cs
+++ Assets/Runtime/Serializator/GameData.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using Balancy.Data;
 using UnityEngine;
 using UnityEngine.Scripting;
 using Yurowm.Coroutines;
